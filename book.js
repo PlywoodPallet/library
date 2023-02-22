0ae038af1dec831d050bcbfa29380d0f0272b2af
PlywoodPallet <25418374+PlywoodPallet@users.noreply.github.com>
2023-02-21 17:49:25 -0800

    Create depreciated function for adding eventlisteners to all remove buttons

--- book.js
+++ book.js
@@ -51,6 +51,23 @@ function addBookToLibrary(e) {
   displayLibrary();
 }
 
+// search all elements for remove buttons and add event listeners
+// function removeBookButtonListener() {
+//   const removeButtons = document.querySelectorAll('div.output button');
+//   console.log(removeButtons);
+
+//   removeButtons.forEach((button) => {
+//     button.addEventListener('click', () => {
+//       const index = button.className;
+
+//       myLibrary.splice(index, 1);
+
+//       // reprint the new library
+//       displayLibrary();
+//     });
+//   });
+// }
+
 const addBookForm = document.querySelector('#addBookForm');
 addBookForm.addEventListener('submit', addBookToLibrary);
 
